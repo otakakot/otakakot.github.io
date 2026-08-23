7e8813c7129a641dd4c9660456f8bf41616da8c5
otakakot <117251387+otakakot@users.noreply.github.com>
2026-08-23 13:01:24 +0900

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://otakakot.github.io',
-});
+  site: 'https://otakakot.github.io',
+})
